d40a018fa204031c5f42c3405737f03904266fff
Kyle Miller <millerkyle85@gmail.com>
2022-03-24 18:24:30 -0700

    fix response

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,8 +1,7 @@
 export default class CurrencyExchange {
-  static async exchangeCurrency(foreignCurrency) {
+  static async exchangeCurrency(foreignCurrency, amount) {
     try {
-      const url = `https://v6.exchangerate-api.com/v6/39dcba4fa5ea9142fe439f2f/pair/USD/${foreignCurrency}`;
-      const response = await fetch(url);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${foreignCurrency}/${amount}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
